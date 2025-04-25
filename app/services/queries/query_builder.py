2df76cbcfe98f0a33db1a99dffe0d889bc2518f0
Max.Thomas <max.thomas@hpi.de>
2025-04-25 15:11:06 +0200

    Removed unused code

--- app/services/queries/query_builder.py
+++ app/services/queries/query_builder.py
@@ -1,11 +1,7 @@
-from app.services.promter.promter_selector import select_all
-
 PROMT = "You are asked to create metadata for a learning resource.\n"
 
 
 def create_query(title: str, description: str, promts: list = None):
-    #if promts is None or len(promts) == 0:
-        #promts = select_all()
     query = PROMT
 
     for p in promts:
