6e5b399a771eec23528d0432656e75981ffb635f
Max.Thomas <max.thomas@hpi.de>
2025-04-25 12:34:59 +0200

    Removed unused code

--- app/services/promter/keyword_promter.py
+++ app/services/promter/keyword_promter.py
@@ -1,5 +1,3 @@
-#import json
-
 PROMT = 'Based on the title and description of the learning resource, give me 10 keywords that ' \
         'describe that paragraph best. The keywords must be common terms that everyone can understand. ' \
         'A keyword may also consist of multiple words. Return the result in form of an Python list ' \
@@ -8,13 +6,3 @@ PROMT = 'Based on the title and description of the learning resource, give me 10
 
 def get_promt():
     return PROMT
-
-
-"""
-def extract_data(raw_data: str):
-    keywords = raw_data.strip("\n").split("= ")[1]
-    keywords = json.loads(keywords)
-    keywords.sort()
-
-    return keywords
-"""
