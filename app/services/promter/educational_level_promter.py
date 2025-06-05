6b330eb67fe4eee38886300a0007df8a499c41ba
Max.Thomas <max.thomas@hpi.de>
2025-06-05 11:33:29 +0200

    Align structure with other promters

--- app/services/promter/educational_level_promter.py
+++ app/services/promter/educational_level_promter.py
@@ -1,12 +1,13 @@
 from app.helper.helper_functions import get_educational_level_framework
 
 
-def get_promt(framework):
-    promt = 'Please give a rating for the difficulty of a learning resource based on the title and the ' \
+PROMT = 'Please give a rating for the difficulty of a learning resource based on the title and the ' \
             'description. Please use the given framework under level_framework and assign ' \
             'the appropriate level. The level is the term or number in the "Level" column. Please ' \
             'select the appropriate level according to the "explanation" column. Return the results ' \
             'in this form: "educationalLevel = <the level you picked according to the framework>".\n'
 
-    return f"{promt}\n" \
+
+def get_promt(framework):
+    return f"{PROMT}\n" \
            f"level_framework: {get_educational_level_framework(framework)}\n\n"
