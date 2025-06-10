7924bc292dcb51bc244691900dc7674ab98e90de
Max.Thomas <max.thomas@hpi.de>
2025-06-10 13:20:52 +0200

    Exclude example for first internal test
    The example for testing while programming is
    removed. Program is ready for first test with
    users.

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,8 +1,8 @@
 import {getFrameworks, getTemplates} from "./dataExchange/templateFrameworkDownloader.js";
 import {storeData} from "./storage/storageHandler.js";
 import {buildUI} from "./ui/uiBuilder.js";
-import {getExample} from "./dataLoader/example/example.js";
-import {loadData} from "./dataLoader/dataLoader.js";
+//import {getExample} from "./dataLoader/example/example.js";
+//import {loadData} from "./dataLoader/dataLoader.js";
 import {catchUpload} from "./dataLoader/uploadCatcher.js";
 
 async function setup() {
@@ -12,8 +12,8 @@ async function setup() {
     storeData(templateData, frameworkData);
     buildUI();
 
-    const example = getExample(1);
-    loadData(example);
+    //const example = getExample(2);  // for testing
+    //loadData(example);
     await catchUpload();
 }
 
