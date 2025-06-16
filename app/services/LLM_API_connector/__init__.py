04ad332aa5c253aebc57f5968fa68eb3abbf9411
Max.Thomas <max.thomas@hpi.de>
2025-06-16 09:44:10 +0200

    Add documentation to gemini_connector.py

---
+++ app/services/LLM_API_connector/__init__.py
@@ -0,0 +1,1 @@
+"""The API connector allows for communication with Large Language Models which are hosted elsewhere."""
--- app/services/LLM_API_connector/gemini_connector.py
+++ app/services/LLM_API_connector/gemini_connector.py
@@ -1,8 +1,19 @@
+"""Executes queries against a GEMINI instance."""
+
+
 import google.generativeai as genai
 import os
 
 
-def start_query(query):
+def start_query(query: str) -> str:
+    """
+    Fires a query against a Google's GEMINI API. Currently, only the model GEMINI Flash 1.5 is supported.
+
+    :param query: The query to be fired against the API.
+    :type query: str
+    :return: A response text as answer for the query.
+    :rtype: str
+    """
     model = genai.GenerativeModel('gemini-1.5-flash')
     genai.configure(api_key=os.environ["GEMINI_KEY"])
 
