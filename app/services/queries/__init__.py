27c29e1c8ae286cc13ea792e0f589d9912019545
Max.Thomas <max.thomas@hpi.de>
2025-06-19 15:55:10 +0200

    Add documentation to the query module

---
+++ app/services/queries/__init__.py
@@ -0,0 +1,1 @@
+"""Assembels all promts into one query to be fired at the LLM."""
--- app/services/queries/query_builder.py
+++ app/services/queries/query_builder.py
@@ -1,7 +1,22 @@
+"""Assembles all promts into a single query for the LLM."""
+
+
 PROMT = "You are asked to create metadata for a learning resource.\n"
 
 
-def create_query(title: str, description: str, promts: list = None):
+def create_query(title: str, description: str, promts: list = None) -> str:
+    """
+    Creates a query based on the title, description, and promts to be passed to a LLM.
+
+    :param title: The title of a course.
+    :type title: str
+    :param description: The description of a course.
+    :type description: str
+    :param promts: A list of promts to be added to the query.
+    :type promts: list
+    :return: A query to be passed to the LLM.
+    :rtype: str
+    """
     query = PROMT
 
     for p in promts:
