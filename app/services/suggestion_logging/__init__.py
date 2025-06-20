864492e27dad573b2a705fc6d13ea8cbf19efc5c
Max.Thomas <max.thomas@hpi.de>
2025-06-20 13:58:05 +0200

    Add documentation to the suggestion_logging module

---
+++ app/services/suggestion_logging/__init__.py
@@ -0,0 +1,1 @@
+"""Tracks suggestions so that there can be an evaluation and quality check."""
--- app/services/suggestion_logging/logger.py
+++ app/services/suggestion_logging/logger.py
@@ -1,8 +1,34 @@
+"""
+Logs and tracks suggestions made by the suggestion engine and saves them to a CSV
+file. This log file can later be used to evaluate the quality of the suggestions
+and to improve this program.
+"""
+
+
 from datetime import datetime
 import os
 
 
-def log_suggestion(course_title, course_description, suggestion_type, framework, suggestions):
+def log_suggestion(course_title: str, course_description: str, suggestion_type: str, framework: str, suggestions: str) \
+        -> None:
+    """
+    Creates a log entry and saves it to a CSV file (log file). The log includes the
+    course title and description for identification of the course. It will create a
+    time stamp when the entry is created. The suggestion type (attribute) will be
+    saved together with the framework used. The actual suggestions will be saved as
+    a string representation.
+
+    :param course_title: The title/name of a course.
+    :type course_title: str
+    :param course_description: The description of the course.
+    :type course_description: str
+    :param suggestion_type: The attribute for which the suggestion is made.
+    :type suggestion_type: str
+    :param framework: The framework used for the suggestion.
+    :type framework: str
+    :param suggestions: The suggestions made by the program.
+    :type suggestions: str
+    """
     if "log.csv" not in os.listdir(os.path.dirname(__file__)):
         header = ",timestamp,course_title,course_description,suggestion_type,framework,suggestions\n"
         with open(os.path.join(os.path.dirname(__file__), "log.csv"), 'w') as f:
