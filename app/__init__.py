3e007b66e1ae3966520ee382ac53512a41e629ba
Max.Thomas <max.thomas@hpi.de>
2025-06-24 09:04:50 +0200

    Add documentation to the routes and the app init

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,16 @@
+"""Initializes the app and registers the endpoints"""
+
+
 from flask import Flask
 
 
-def create_app():
+def create_app() -> Flask:
+    """
+    Creates the Flask application object and registers the routes.
+
+    :return: The Flask application.
+    :rtype: Flask
+    """
     app = Flask(__name__, static_folder='static')
 
     from .routes import main as main_blueprint
