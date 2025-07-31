8192f15f639da848ca936247c11713bbe7c66fb9
Max.Thomas <max.thomas@hpi.de>
2025-07-31 10:52:17 +0200

    Change GEMINI API and Model
    The API used to this point is deprecated and will be
    discontinued by the end of August. It was therefor
    upgraded to the currently supported API. In the same
    instance the now deprecated model GEMINI 1.5 flash was
    replaced by GEMINI 2.0 flash.

--- app/services/LLM_API_connector/gemini_connector.py
+++ app/services/LLM_API_connector/gemini_connector.py
@@ -1,23 +1,24 @@
 """Executes queries against a GEMINI instance."""
 
 
-import google.generativeai as genai
+from google import genai
 import os
 
 
 def start_query(query: str) -> str:
     """
-    Fires a query against a Google's GEMINI API. Currently, only the model GEMINI Flash 1.5 is supported.
+    Fires a query against a Google's GEMINI API. Currently, only the model GEMINI 2.0 Flash is supported.
 
     :param query: The query to be fired against the API.
     :type query: str
     :return: A response text as answer for the query.
     :rtype: str
     """
-    model = genai.GenerativeModel('gemini-1.5-flash')
-    genai.configure(api_key=os.environ["GEMINI_KEY"])
+    client = genai.Client(api_key=os.environ["GEMINI_API_KEY"])
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents=query
+    )
 
-    response = model.generate_content(query)
     text = response.text
 
     return text
